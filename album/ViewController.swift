dfdabf3db133a324d0c97d79a59eb3180fbd8443
hiyoko <hiyoko@hiyoko-no-macbook.local>
2019-02-15 12:11:35 +0900

    Initial Commit

---
+++ album/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  album
+//
+//  Created by hiyoko on 2019/02/15.
+//  Copyright © 2019年 hiyoko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
